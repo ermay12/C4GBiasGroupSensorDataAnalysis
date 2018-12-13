5d22c193727f4ae250d02cc0975929dcf73ab623
ermay12 <ermay12@gmail.com>
2018-12-13 07:32:03 -0500

    updates to final package

--- Sensors/Analysis/HeartRateAnalysis.py
+++ Sensors/Analysis/HeartRateAnalysis.py
@@ -12,6 +12,10 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
 import array as arr
+import datetime
+import threading
+import serial
+import time
 
 # represents a single data point read from the arduino.  (time (ns), ecg (raw), gsr (raw))
 # time starts at 0 at the beginning of the image.
@@ -111,7 +115,7 @@ def computeBPMs(currentStimuliData):
     return (bpmTimes, bpms)
 
 #build model using only data categorized as positive or negative stimuli data
-def buildModel(participantData):
+def buildModel(participantsData):
     classify = svm.SVC(kernel="rbf")
 
     #extract features and labels from data
@@ -141,21 +145,29 @@ def buildModel(participantData):
     labels = np.asarray(labels)
     xfeatures = []
     yfeatures = []
+    afeatures = []
+    bfeatures = []
     for feature in features:
         xfeatures.append(feature[0])
         yfeatures.append(feature[1])
-    #print(xfeatures)
-    #print(yfeatures)
+        afeatures.append(feature[2])
+        bfeatures.append(feature[3])
+    print(xfeatures)
+    print(yfeatures)
+    print(afeatures)
+    print(bfeatures)
     plt.plot(xfeatures, yfeatures, "yo")
     plt.show()
     train_features, test_features, train_labels, test_labels = tts(features, labels, test_size=0.2)
     classify.fit(train_features, train_labels)
     predictions = classify.predict(test_features)
+    print(test_features)
     #print(classify.coef_)
     #print("Predictions: ", predictions)
     scores = cross_val_score(classify, features, labels, cv=8)
     #print(scores)
     #print(scores.mean(), scores.std())
+    print(predictions)
 
     """
     print("For KFold \n")
@@ -172,140 +184,288 @@ def buildModel(participantData):
 
     """
 
+def extractFeaturesLabels(participantsData):
+    features = []
+    labels = []
+    pKeys = participantsData.keys()
+    for pKey in pKeys:
+        participant = participantsData[pKey]
+        positive = participant.positiveStimuliData
+        negative = participant.negativeStimuliData
+        pSKeys = positive.keys()
+        nSKeys = negative.keys()
+        for pSKey in pSKeys:
+            imageData = positive[pSKey]
+            features.append([imageData.normalizedHeartRateRegressionSlope, imageData.normalizedGSRRegressionSlope,
+                             imageData.normalizedHeartRateRegressionYIntercept,
+                             imageData.normalizedGSRRegressionYIntercept])
+            labels.append(1)
+        for nSKey in nSKeys:
+            imageData = negative[nSKey]
+            features.append([imageData.normalizedHeartRateRegressionSlope, imageData.normalizedGSRRegressionSlope,
+                             imageData.normalizedHeartRateRegressionYIntercept,
+                             imageData.normalizedGSRRegressionYIntercept])
+            labels.append(0)
+
+    # build model
+    features = np.asarray(features)
+    labels = np.asarray(labels)
+    return features, labels
+
+def extractSingleFileFeatures(stimuliData):
+    features = []
+    features.append([stimuliData.normalizedHeartRateRegressionSlope, stimuliData.normalizedGSRRegressionSlope,
+                     stimuliData.normalizedHeartRateRegressionYIntercept,
+                     stimuliData.normalizedGSRRegressionYIntercept])
+    features = np.asarray(features)
+    return features
+
+def parseSingleFile(fileName):
+    file = open(join(os.getcwd(), fileName), 'r')
+    fileName = fileName.replace("/", "\\").split('\\')[len(fileName.split('\\')) - 1]
+    fileName = fileName.replace('.txt', '')
+    currentParticipant = ParticipantData(fileName)
+    for line in file.readlines():
+        # insect correspond to negative data, flower correspond to positive data, both used to build model
+        # unknown correspond to uncategorized data, used to test using model
+        if '.jpg' in line or '#' in line:
+            imageName = line.replace('.jpg', '').replace('#', '')
+            if ('insect' in imageName):
+                currentStimuliData = StimuliData(imageName)
+                currentParticipant.negativeStimuliData[imageName] = currentStimuliData
+            elif ('flower' in imageName):
+                currentStimuliData = StimuliData(imageName)
+                currentParticipant.positiveStimuliData[imageName] = currentStimuliData
+            else:
+                currentStimuliData = StimuliData(imageName)
+                currentParticipant.unknownStimuliData[imageName] = currentStimuliData
+            startTime = -1
+
+        if len(line.split(',')) == 3:
+            if (startTime == -1):
+                startTime = int(line.split(',')[0])
+            currentStimuliData.rawData.append(DataValue(startTime, line))
+
+    # process data for each stimuli
+    for currentStimuliData in set(currentParticipant.positiveStimuliData.values()) | \
+                              set(currentParticipant.negativeStimuliData.values()) | \
+                              set(currentParticipant.unknownStimuliData.values()):
+        currentRawData = currentStimuliData.rawData
+
+        # get data from raw data
+        ecgs = []
+        gsrs = []
+        times = []
+        for d in currentRawData:
+            ecgs.append(d.ecg)
+            gsrs.append(d.gsr)
+            times.append(d.time)
+
+        # compute ecg data
+        currentStimuliData.avgHeartRate = sum(ecgs) / len(ecgs)
+        currentStimuliData.stdDevHeartRate = statistics.stdev(ecgs)
+        currentStimuliData.minHeartRate = min(ecgs)
+        currentStimuliData.maxHeartRate = max(ecgs)
+        (bpmTimes, bpms) = computeBPMs(currentStimuliData)
+        # plot data
+
+        (bpmTimes, bpms) = computeBPMs(currentStimuliData)
+        plt.figure(1)
+        plt.subplot(211)
+        plt.plot(times, ecgs)
+
+        plt.subplot(212)
+        plt.plot(bpmTimes, bpms)
+        plt.title(currentParticipant.name + " " + currentStimuliData.name)
+        plt.show()
+
+        # ---call compute bpm function---#
+        #bpmTimes, bpms = [0, 1, 2, 3, 4, 5, 6, 7], [300, 400, 500, 600, 500, 300, 200, 100]
+        # compute normalized bpms
+        normalizedBpms = []
+        for bpm in bpms:
+            # uses newValue = (value - ave)/std
+            normalizedBpms.append((bpm - currentStimuliData.avgHeartRate) / currentStimuliData.stdDevHeartRate)
+        currentStimuliData.heartRateTimes = bpmTimes
+        currentStimuliData.heartRates = normalizedBpms
+
+        # compute gsr data
+        currentStimuliData.avgGSR = sum(gsrs) / len(gsrs)
+        currentStimuliData.stdDevGSR = statistics.stdev(gsrs)
+        currentStimuliData.minGSR = min(gsrs)
+        currentStimuliData.maxGSR = max(gsrs)
+        # compute normalized gsrs
+        normalizedGSRs = []
+        for gsr in gsrs:
+            # uses newValue = (value - ave)/std
+            if currentStimuliData.stdDevGSR == 0:
+                normalizedGSRs.append(0)
+            else:
+                normalizedGSRs.append((gsr - currentStimuliData.avgGSR) / currentStimuliData.stdDevGSR)
+
+        # add ave heart rate data for each stimuli to participant's data
+        currentParticipant.avgHeartRates.append(currentStimuliData.avgHeartRate)
+        currentParticipant.stdDevHeartRates.append(currentStimuliData.stdDevHeartRate)
+
+        # add ave gsr data for each stimuli to participant's data
+        currentParticipant.avgGSRs.append(currentStimuliData.avgGSR)
+        currentParticipant.stdDevGSRs.append(currentParticipant.stdDevGSR)
+
+        # compute lin reg bpm for each stiumli
+        heartRateRegSlope, heartRateRegIntercept = np.polyfit(bpmTimes, normalizedBpms, 1)
+        currentStimuliData.normalizedHeartRateRegressionSlope = heartRateRegSlope
+        currentStimuliData.normalizedHeartRateRegressionYIntercept = heartRateRegIntercept
+
+        # graph lin reg bpm
+        """
+        plt.plot(np.asarray(bpmTimes), np.asarray(normalizedBpms), 'yo', np.asarray(bpmTimes),
+                 heartRateRegSlope * np.asarray(bpmTimes) + heartRateRegIntercept, '--k')
+        plt.title(fileName + ": " + imageName)
+        plt.xlabel('Time')
+        plt.ylabel('BPM')
+        plt.show()
+        """
+        # compute lin reg gsr for each stimuli
+        gsrRegSlope, gsrRegIntercept = np.polyfit(times, normalizedGSRs, 1)
+        currentStimuliData.normalizedGSRRegressionSlope = gsrRegSlope
+        currentStimuliData.normalizedGSRRegressionYIntercept = gsrRegIntercept
+
+        # graph lin reg gsr
+        """
+        plt.plot(np.asarray(times), np.asarray(normalizedGSRs), 'yo', np.asarray(times),
+                 gsrRegSlope * np.asarray(times) + gsrRegIntercept, '--k')
+        plt.title(fileName + ": " + imageName)
+        plt.xlabel('Time')
+        plt.ylabel('GSR')
+        plt.show()
+        """
+    # compute summary data of each participant
+    currentParticipant.avgHeartRate = sum(currentParticipant.avgHeartRates) / len(currentParticipant.avgHeartRates)
+    currentParticipant.stdDevHeartRate = statistics.stdev(currentParticipant.avgHeartRates)
+    currentParticipant.avgGSR = sum(currentParticipant.avgGSRs) / len(currentParticipant.avgGSRs)
+    currentParticipant.stdDevGSR = statistics.stdev(currentParticipant.avgGSRs)
+    file.close()
+    return currentParticipant
+
 #processes all the data in the dataDirectory folder and returns a list of ParticipantData
 def parseData(dataDirectory):
     dataDirectory = join(os.getcwd(), dataDirectory)
     participantsData = {}
     for fileName in os.listdir(dataDirectory):
-        if('.txt' not in fileName):
+        if '.txt' not in fileName or 'participant' in fileName:
             continue
-        file = open(dataDirectory + "/" + fileName, 'r')
-        fileName = fileName.split('\\')[len(fileName.split('\\'))-1]
-        fileName = fileName.replace('.txt', '')
-        currentParticipant = ParticipantData(fileName)
+        currentParticipant = parseSingleFile(join('data',fileName))
         participantsData[fileName] = currentParticipant
-        for line in file.readlines():
-            #insect correspond to negative data, flower correspond to positive data, both used to build model
-            #unknown correspond to uncategorized data, used to test using model
-            if '.jpg' in line or '#' in line:
-                imageName = line.replace('.jpg', '').replace('#', '')
-                if('insect' in imageName):
-                    currentStimuliData = StimuliData(imageName)
-                    currentParticipant.negativeStimuliData[imageName] = currentStimuliData
-                elif('flower' in imageName):
-                    currentStimuliData = StimuliData(imageName)
-                    currentParticipant.positiveStimuliData[imageName] = currentStimuliData
-                else:
-                    currentStimuliData = StimuliData(imageName)
-                    currentParticipant.unknownStimuliData[imageName] = currentStimuliData
-                startTime = -1
 
-            if len(line.split(',')) == 3:
-                if (startTime == -1):
-                    startTime = int(line.split(',')[0])
-                currentStimuliData.rawData.append(DataValue(startTime, line))
-
-        #process data for each stimuli
-        for currentStimuliData in set(currentParticipant.positiveStimuliData.values()) \
-                | set(currentParticipant.negativeStimuliData.values())\
-                | set(currentParticipant.unknownStimuliData.values()):
-            currentRawData = currentStimuliData.rawData
-
-            # get data from raw data
-            ecgs = []
-            gsrs = []
-            times = []
-            for d in currentRawData:
-                ecgs.append(d.ecg)
-                gsrs.append(d.gsr)
-                times.append(d.time)
-
-            #compute ecg data
-            currentStimuliData.avgHeartRate = sum(ecgs) / len(ecgs)
-            currentStimuliData.stdDevHeartRate = statistics.stdev(ecgs)
-            currentStimuliData.minHeartRate = min(ecgs)
-            currentStimuliData.maxHeartRate = max(ecgs)
-
-            #plot data
-            (bpmTimes, bpms) = computeBPMs(currentStimuliData)
-            plt.figure(1)
-            plt.subplot(211)
-            plt.plot(times, ecgs)
-
-            plt.subplot(212)
-            plt.plot(bpmTimes, bpms)
-            plt.title(currentParticipant.name + " " + currentStimuliData.name)
-            plt.show()
-
-            #---call compute bpm function---#
-            bpmTimes, bpms = [0, 1, 2, 3, 4, 5, 6, 7], [300, 400, 500, 600, 500, 300, 200, 100]
-            #compute normalized bpms
-            normalizedBpms = []
-            for bpm in bpms:
-                #uses newValue = (value - ave)/std
-                normalizedBpms.append((bpm - currentStimuliData.avgHeartRate)/currentStimuliData.stdDevHeartRate)
-            currentStimuliData.heartRateTimes = bpmTimes
-            currentStimuliData.heartRates = normalizedBpms
-
-            #compute gsr data
-            currentStimuliData.avgGSR = sum(gsrs) / len(gsrs)
-            currentStimuliData.stdDevGSR = statistics.stdev(gsrs)
-            currentStimuliData.minGSR = min(gsrs)
-            currentStimuliData.maxGSR = max(gsrs)
-            #compute normalized gsrs
-            normalizedGSRs = []
-            for gsr in gsrs:
-                # uses newValue = (value - ave)/std
-                normalizedGSRs.append((gsr - currentStimuliData.avgGSR)/currentStimuliData.stdDevGSR)
-
-            #add ave heart rate data for each stimuli to participant's data
-            currentParticipant.avgHeartRates.append(currentStimuliData.avgHeartRate)
-            currentParticipant.stdDevHeartRates.append(currentStimuliData.stdDevHeartRate)
-
-            #add ave gsr data for each stimuli to participant's data
-            currentParticipant.avgGSRs.append(currentStimuliData.avgGSR)
-            currentParticipant.stdDevGSRs.append(currentParticipant.stdDevGSR)
-
-            #compute lin reg bpm for each stiumli
-            heartRateRegSlope, heartRateRegIntercept = np.polyfit(bpmTimes, normalizedBpms, 1)
-            currentStimuliData.normalizedHeartRateRegressionSlope = heartRateRegSlope
-            currentStimuliData.normalizedHeartRateRegressionYIntercept = heartRateRegIntercept
-
-            #graph lin reg bpm
-            """
-            plt.plot(np.asarray(bpmTimes), np.asarray(normalizedBpms), 'yo', np.asarray(bpmTimes),
-                     heartRateRegSlope * np.asarray(bpmTimes) + heartRateRegIntercept, '--k')
-            plt.title(fileName + ": " + imageName)
-            plt.xlabel('Time')
-            plt.ylabel('BPM')
-            plt.show()
-            """
-            #compute lin reg gsr for each stimuli
-            gsrRegSlope, gsrRegIntercept = np.polyfit(times, normalizedGSRs, 1)
-            currentStimuliData.normalizedGSRRegressionSlope = gsrRegSlope
-            currentStimuliData.normalizedGSRRegressionSlope = gsrRegIntercept
-
-            #graph lin reg gsr
-            """
-            plt.plot(np.asarray(times), np.asarray(normalizedGSRs), 'yo', np.asarray(times),
-                     gsrRegSlope * np.asarray(times) + gsrRegIntercept, '--k')
-            plt.title(fileName + ": " + imageName)
-            plt.xlabel('Time')
-            plt.ylabel('GSR')
-            plt.show()
-            """
-        #compute summary data of each participant
-        currentParticipant.avgHeartRate = sum(currentParticipant.avgHeartRates)/len(currentParticipant.avgHeartRates)
-        currentParticipant.stdDevHeartRate = statistics.stdev(currentParticipant.avgHeartRates)
-        currentParticipant.avgGSR = sum(currentParticipant.avgGSRs) / len(currentParticipant.avgGSRs)
-        currentParticipant.stdDevGSR = statistics.stdev(currentParticipant.avgGSRs)
 
     return participantsData
 
-participantsData = parseData("/Users/jadewang/Documents/CMU/Sophomore/C4G Bias/C4GBiasGroupSensorDataAnalysis/Sensors/Analysis/data")
+dataFile = 0
+ser = 0
+terminateThread = False
+threadExecuting = False
+dataFileName = ''
 
-buildModel(participantsData)
+class CollectDataThread (threading.Thread):
+   def __init__(self):
+      threading.Thread.__init__(self)
 
+   def run(self):
+       global dataFile
+       global ser
+       global terminateThread
+       global threadExecuting
+       while True:
+           if ser.in_waiting > 0:
+               dataFile.write(ser.readline().decode("utf-8") )
+               if (terminateThread):
+                   threadExecuting = False
+                   return
+
+def initSensors():
+    global dataFile
+    global ser
+    global dataFileName
+    ser = serial.Serial(port="COM5", baudrate=19200)
+    dataFileName = join("data", "participant","participantData%s.txt" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(':','').replace(' ', ''))
+    dataFile = open(join(os.getcwd(),dataFileName), "w")
+    time.sleep(3)
+
+def stimuliStart(stimuliCategory):
+    global dataFile
+    global ser
+    global terminateThread
+    global threadExecuting
+    dataFile.write("#")
+    dataFile.write(stimuliCategory)
+    dataFile.write("\n")
+    terminateThread = False
+    threadExecuting = True
+    collectDataThread = CollectDataThread()
+    collectDataThread.start()
+    ser.write(b'f')
+
+def stimuliEnd():
+    global dataFile
+    global ser
+    global terminateThread
+    ser.write(b'f')
+    terminateThread = True
+    while threadExecuting:
+        pass
+    while ser.in_waiting > 0:
+        dataFile.write(ser.readline().decode("utf-8") )
+
+    dataFile.write('endOfStimuli\n')
+
+def computeBias():
+    global dataFile
+    global ser
+    global dataFileName
+    dataFile.close()
+    ser.close()
+    participantData = parseSingleFile(dataFileName)
+
+    #build model
+    participantsData = parseData("data")
+    features, labels = extractFeaturesLabels(participantsData)
+    classify = svm.SVC(kernel="rbf")
+    classify.fit(features, labels)
+
+    #Test model here
+    stimuliCategories = {}
+
+    for stimuliData in participantData.unknownStimuliData.values():
+        singleFeatures = extractSingleFileFeatures(stimuliData)
+        result = classify.predict(singleFeatures)
+        if(result[0] == 0):
+            stimuliCategories[stimuliData.name] = True
+        else:
+            stimuliCategories[stimuliData.name] = False
+    ################
+    return stimuliCategories
+    #return a map of "stimuliCategory"-> bias:boolean
+
+
+
+#participantsData = parseData("data")
+
+#buildModel(participantsData)
+
+initSensors()
+
+stimuliStart("Category 1")
+time.sleep(3)
+stimuliEnd()
+
+stimuliStart("Category 2")
+time.sleep(3)
+stimuliEnd()
+
+categories = computeBias()
+
+for key in categories.keys():
+    print(key, categories[key])
 
 #below this is dead code.  please disregard
 '''
